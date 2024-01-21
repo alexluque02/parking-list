29d8e3579e9455de5b0ccf9c035d3b95f8dffcb0
alexluque02 <alexanderluquehoffrogge@gmail.com>
2024-01-21 17:02:38 +0100

    Ejercicio hecho

--- lib/widget/parking_item.dart
+++ lib/widget/parking_item.dart
@@ -7,6 +7,55 @@ class ParkingItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(parking.nombre!);
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                bottomLeft: Radius.circular(20.0),
+              ),
+              child: Image.network(
+                'https://viajes.nationalgeographic.com.es/medio/2023/09/12/plaza-del-ayuntamiento-valencia_aa6b85c7_230912183353_800x800.jpg',
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    parking.nombre!,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.location_on),
+                      Expanded(
+                        child: Text(
+                          parking.direccion!,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Agrega aquí más widgets de texto u otros elementos según sea necesario.
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
